c0e0b5793fac3a9b03a5c62207d639f2ab07a7a2
ssrivas8 <ssrivas8@ncsu.edu>
2016-10-25 03:39:56 -0400

    updated script
    removed non existing tables

--- Database Script/dropTables.sql
+++ Database Script/dropTables.sql
@@ -1,15 +1,12 @@
 drop table SICK_PERSON;
 drop table WELL_PERSON;
 drop table RECORD_DISEASE;
-drop table DISEASE_OBSERVATION;
 drop table STANDARD_RECOMMENDATION;
 drop table SPECIFIC_RECOMMENDATION;
 drop table ALERT;
-drop table RECORD_OBSERVATION;
 drop table PERSON;
 drop table DISEASE;
 drop table OBSERVATION;
-drop table OBSERVATION_TYPE;
 drop table RECOMMENDATION;
 
 COMMIT;
