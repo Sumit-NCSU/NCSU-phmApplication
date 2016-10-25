776de2910f204d34407a7cd01f67ecdfd86fd7b9
ssrivas8 <ssrivas8@ncsu.edu>
2016-10-25 01:05:05 -0400

    updated trigger
    updated trigger

--- Database Script/TRIGGER_DELETE_WELL_PERSON.sql
+++ Database Script/TRIGGER_DELETE_WELL_PERSON.sql
@@ -1,9 +1,9 @@
-CREATE OR REPLACE TRIGGER delete_well_person BEFORE
-    DELETE ON sick_person
+create or replace TRIGGER delete_well_person AFTER
+    INSERT ON sick_person
     FOR EACH ROW
 BEGIN
     DELETE FROM well_person WHERE
-        p_id =:old.p_id;
-
+        p_id =:new.p_id;
+    COMMIT;
 END;
 /
