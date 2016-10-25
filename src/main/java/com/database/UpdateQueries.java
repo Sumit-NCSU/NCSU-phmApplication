4c8be83f4a5d321f9a62fc166e78b89674a3d787
ssrivas8 <ssrivas8@ncsu.edu>
2016-10-25 05:53:36 -0400

    updated alert screen

--- src/main/java/com/database/UpdateQueries.java
+++ src/main/java/com/database/UpdateQueries.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package com.database;
 
 /**
@@ -8,5 +5,19 @@ package com.database;
  *
  */
 public class UpdateQueries {
+
+	/**
+	 * Method to update the alert table in database and set the is viewed column
+	 * as true. If flag clear mandatory is set then mandatory alerts are also
+	 * cleared.
+	 * 
+	 * @param clearMandatory
+	 *            if mandatory alerts need to be cleared as well.
+	 * @return boolean value indicating that alerts were cleared successfully
+	 */
+	public static boolean clearAlerts(boolean clearMandatory) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
