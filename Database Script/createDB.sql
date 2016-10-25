a1fb15acae56be1d28247ee2dc31f975b31b131d
nikhilsharma93 <nikhilsharma.22.93@gmail.com>
2016-10-25 06:50:28 -0400

    updating

--- Database Script/createDB.sql
+++ Database Script/createDB.sql
@@ -2,6 +2,8 @@ exec CreateTables.sql
 exec TRIGGER_OBSV_BOUND.sql
 exec TRIGGER_DELETE_WELL_PERSON.sql
 exec TRIGGER_RECORD_WELL_PERSON.sql
+exec TRIGGER_OBSERVATION_VIEWED.sql
+exec PROCEDURE_CHECK_FREQ.sql
 exec insertSampleData.sql
 
 commit;
