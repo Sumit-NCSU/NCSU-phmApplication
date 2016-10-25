693456dd117c6d5451cce04569b9afe412a20b48
asinsinwal <assinsin@ncsu.edu>
2016-10-25 02:58:11 -0400

    Updating Insert Query

--- src/main/java/com/ui/NewPhmUser.java
+++ src/main/java/com/ui/NewPhmUser.java
@@ -53,7 +53,7 @@ public class NewPhmUser {
 			System.out.println("Enter Gender: ");
 			String gender = sc.nextLine();
 			// TODO: auto-generate person ID?
-			PersonDTO person = new PersonDTO("phmseq.nextval", name, username, password, address, dob, gender);
+			PersonDTO person = new PersonDTO(null, name, username, password, address, dob, gender);
 			boolean status = insertPerson(person);
 			if (status) {
 				System.out.println("Account Created Successfully");
