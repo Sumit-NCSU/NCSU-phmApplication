0843fed85398d1b072bd51bca4ef080ecce74e5a
ssrivas8 <ssrivas8@ncsu.edu>
2016-10-24 23:57:25 -0400

    record well person trigger
    trigger for creating a record in well person table whenever a new record in person table is created.

---
+++ Database Script/RECORD_WELL_PERSON.sql
@@ -0,0 +1,20 @@
+CREATE OR REPLACE TRIGGER record_well_person AFTER
+    INSERT ON person
+    FOR EACH ROW
+BEGIN
+    INSERT INTO well_person (
+        p_id,
+        hs1_id,
+        hs2_id,
+        hs1_auth_date,
+        hs2_auth_date
+    ) VALUES (
+        :new.p_id,
+        NULL,
+        NULL,
+        NULL,
+        NULL
+    );
+
+END;
+/
