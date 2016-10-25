d7df4866c98e489ea736799848069969e682031e
nikhilsharma93 <nikhilsharma.22.93@gmail.com>
2016-10-25 02:30:52 -0400

    updating

--- Database Script/TRIGGER_OBSV_BOUND.sql
+++ Database Script/TRIGGER_OBSV_BOUND.sql
@@ -5,34 +5,33 @@ create or replace TRIGGER obsv_value_bound
   ON observation
 FOR EACH ROW
 declare is_null number;
-lo_bound number;
-up_bound number;
+lo_bound varchar2(20);
+up_bound varchar2(20);
 alert_count number;
 descrip_reco VARCHAR2(1024);
 is_sick number;
+ob_frequency varchar2(20);
 BEGIN
 select R1.lower_bound into is_null from RECOMMENDATION R1 where R1.r_id = :new.r_id;
 if is_null is not null
   then
-    select to_number(R1.lower_bound) into lo_bound from RECOMMENDATION R1 where R1.r_id = :new.r_id;
-    select to_number(R1.upper_bound) into up_bound from RECOMMENDATION R1 where R1.r_id = :new.r_id;
-    select R1.description into descrip_reco from RECOMMENDATION R1 where R1.r_id = :new.r_id;
-    if (to_number(:new.ob_value) > up_bound or to_number(:new.ob_value) < lo_bound)
+    select to_number(R1.lower_bound), to_number(R1.upper_bound), R1.description, R1.frequency into lo_bound, up_bound, descrip_reco, ob_frequency from RECOMMENDATION R1 where R1.r_id = :new.r_id;
+    if (to_number(:new.ob_value) > to_number(up_bound) or to_number(:new.ob_value) < to_number(lo_bound))
       then
         select count(*) into alert_count from alert;
         select count(*) into is_sick from sick_person where p_id = :new.p_id;
         if is_sick = 1
           then
-            insert into alert values(alert_count+1,:new.p_id, descrip_reco, 'T', 'F');
+            insert into alert values(alert_count+1,:new.p_id, descrip_reco, :new.ob_time, :new.ob_value, ob_frequency, lo_bound, up_bound,  'T', 'F');
           else
-            insert into alert values(alert_count+1,:new.p_id, descrip_reco, 'T', 'F');
+            insert into alert values(alert_count+1,:new.p_id, descrip_reco, :new.ob_time, :new.ob_value, ob_frequency, lo_bound, up_bound, 'F', 'F');
         end if;
-        --DBMS_OUTPUT.PUT_LINE('InsertingYY');
-        --DBMS_OUTPUT.PUT_LINE('InsertingNNN');
     end if;
 end if;
 END;
 /
 
 
+
 COMMIT;
+
--- Database Script/insertSampleData.sql
+++ Database Script/insertSampleData.sql
@@ -1,29 +1,7 @@
---Person
-insert into Person values('P1', 'Sheldon Cooper', null, 'password', ' 2500 Sacramento, Apt 903, Santa Cruz, CA - 90021', to_date('19840526','YYYYMMDD'),'M');
-insert into Person values('P2', 'Leonard Hofstader', null, 'password', ' 2500 Sacramento, Apt 904, Santa Cruz, CA - 90021', to_date('19890419','YYYYMMDD'),'M');
-insert into Person values('P3', 'Penny Hofstader', null, 'password', ' 2500 Sacramento, Apt 904, Santa Cruz, CA - 90021', to_date('19901225','YYYYMMDD'),'F');
-insert into Person values('P4', 'Amy Farrahfowler', null, 'password', ' 2500 Sacramento, Apt 905, Santa Cruz, CA - 90021', to_date('19920615','YYYYMMDD'),'F');
-
-
---Sick Person
-insert into SICK_PERSON values('P1', 'P2', 'P4', to_date('20161021','YYYYMMDD'), to_date('20161021','YYYYMMDD'));
-insert into SICK_PERSON values('P2', 'P3', null, to_date('20161009','YYYYMMDD'), null);
-
-
---Well Person
-update  WELL_PERSON set HS1_id = 'P4', HS1_auth_date = to_date('20161021','YYYYMMDD') where p_id = 'P3'; 
---insert into WELL_PERSON values('P4', null, null, null, null); 
-
-
 --Disease
 insert into Disease values(1,'Heart Disease');
 insert into Disease values(2, 'HIV');
 insert into Disease values(3, 'COPD');
-
-
---Record Disease
-insert into RECORD_DISEASE values('P1', 1, '22-OCT-16');
-insert into RECORD_DISEASE values('P2', 2, '10-OCT-16');
 
 
 --RECOMMENDATION
@@ -52,13 +30,39 @@ insert into STANDARD_RECOMMENDATION values(3, 8);
 insert into STANDARD_RECOMMENDATION values(3, 9);
 
 
+
+--Person
+insert into Person values('P1', 'Sheldon Cooper', 'moon_pie', 'password', ' 2500 Sacramento, Apt 903, Santa Cruz, CA - 90021', to_date('19840526','YYYYMMDD'),'M');
+insert into Person values('P2', 'Leonard Hofstader', 'leo', 'password', ' 2500 Sacramento, Apt 904, Santa Cruz, CA - 90021', to_date('19890419','YYYYMMDD'),'M');
+insert into Person values('P3', 'Penny Hofstader', 'cheese_cake', 'password', ' 2500 Sacramento, Apt 904, Santa Cruz, CA - 90021', to_date('19901225','YYYYMMDD'),'F');
+insert into Person values('P4', 'Amy Farrahfowler', 'fowl', 'password', ' 2500 Sacramento, Apt 905, Santa Cruz, CA - 90021', to_date('19920615','YYYYMMDD'),'F');
+
+
+--Sick Person
+insert into SICK_PERSON values('P1', 'P2', 'P4', to_date('20161021','YYYYMMDD'), to_date('20161021','YYYYMMDD'));
+insert into SICK_PERSON values('P2', 'P3', null, to_date('20161009','YYYYMMDD'), null);
+
+
+--Well Person
+update  WELL_PERSON set HS1_id = 'P4', HS1_auth_date = to_date('20161021','YYYYMMDD') where p_id = 'P3'; 
+--insert into WELL_PERSON values('P4', null, null, null, null); 
+
+
+
+--Record Disease
+insert into RECORD_DISEASE values('P1', 1, '22-OCT-16');
+insert into RECORD_DISEASE values('P2', 2, '10-OCT-16');
+
+
+
+
 --Specific Recommendation
 insert into SPECIFIC_RECOMMENDATION values('P2', 10);
 insert into SPECIFIC_RECOMMENDATION values('P2', 5);
 insert into SPECIFIC_RECOMMENDATION values('P2', 6);
 insert into SPECIFIC_RECOMMENDATION values('P2', 7);
-insert into SPECIFIC_RECOMMENDATION values('P3', 1); -- For well patient
-insert into SPECIFIC_RECOMMENDATION values('P4', 1); -- For well patient
+--insert into SPECIFIC_RECOMMENDATION values('P3', 1); -- For well patient
+--insert into SPECIFIC_RECOMMENDATION values('P4', 1); -- For well patient
 
 
 --Observation
